bf0db3a2ddbd6975bc07db9631fc842eb4da4e29
swayampandey <swayam265@gmail.com>
2022-06-21 14:15:31 +0530

    this is demo file

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Swayam How are you?");
+		System.out.println("Welcome to java");
+		// TODO Auto-generated method stub
+
+	}
+
+}
